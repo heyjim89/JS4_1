0ad2141a66c6da82fad7a6d0f307e73ea85e65e1
Jim D <heyjim89@gmail.com>
2015-06-23 09:30:15 -0600

    Jquery Appends Start

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-$( "#buttons" ).click(function() {
-  console.log("Button has been clicked");
+$(document).ready(function() {
+
+	$( '.add' ).click(function() {
+	  // console.log("Button has been clicked");
+	  $('body').append('<p>Hello World</p>');
+	});
+	$( '.h1' ).click(function() {
+	  // console.log("Button has been clicked");
+	  $('body').append('<h1>Goodbye World</h1>');
+	});
+	$( '.buttons' ).click(function() {
+	  // console.log("Button has been clicked");
+	  $('body').append('<h1>Goodbye World</h1>');
+	});
 });
