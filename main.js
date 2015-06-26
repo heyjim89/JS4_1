87c76ea76bc1e016ed33c365ff47f1a00e6b4c42
Jim D <heyjim89@gmail.com>
2015-06-26 11:45:01 -0600

    Fixed popup that broke

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
+    
+    $('.add').click(function() {
+        console.log("Button has been clicked");
+        $('body').append('<p>Hello World</p>');
+    });
+    $('.h1').click(function() {
 
-	$( '.add' ).click(function() {
-	  console.log("Button has been clicked");
-	  $('body').append('<p>Hello World</p>');
-	});
-	$( '.h1' ).click(function() {
+        $('body').append('<h1>Goodbye World</h1>');
+    });
+    $('.ol').click(function() {
 
-	  $('body').append('<h1>Goodbye World</h1>');
-	});
-	$( '.ol' ).click(function() {
-
-	  $('body').append('<ol><li>Test1</li><li>Test2</li><li>Test3</li></ol>');
-	});
+        $('body').append('<ol><li>Test1</li><li>Test2</li><li>Test3</li></ol>');
+    });
 
 
-	// PART TWO BEGIN
-	
-	$('p').mouseover(function() {
+    // PART TWO BEGIN
+
+    $('p').mouseover(function() {
 
         $(this).css({
-            'color' : 'pink',
+            'color': 'pink',
         });
     });
 
     $('p').mouseout(function() {
 
         $(this).css({
-            'color' : 'Black',
+            'color': 'Black',
         });
     });
 
@@ -37,12 +37,10 @@ $(document).ready(function() {
 
     $('a').click(function() {
 
-      if (confirm("Are you sure you want to go?")) {
-      }
-      else {
-	  	$(this).remove();
-	  	return false;
-      }
+        if (confirm("Are you sure you want to go?")) {} else {
+            $(this).remove();
+            return false;
+        }
     });
 
     // PART THREE BEGIN
@@ -53,10 +51,10 @@ $(document).ready(function() {
     });
 
 
-	$( "body" ).on( "click", ".close", function( event ) {
-	    event.preventDefault();
-    	$('.close').remove();
-    	$('.popup').remove();
-	});
+    $("body").on("click", ".close", function(event) {
+        event.preventDefault();
+        $('.close').remove();
+        $('.popup').remove();
+    });
 
 });
